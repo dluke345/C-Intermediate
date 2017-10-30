8b9ef3ce5c2ef546a2fae8a4f1993986504a1c98
David Luke <dluke345@gmail.com>
2017-10-30 14:24:52 -0500

    Add files via upload

---
+++ AccessModifiers/Amazon/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            //var rating = CalculateRating(excludeOrders: true);
+
+            var calculator = new RateCalculator();
+            var rating = calculator.Calculate(this);
+
+            Console.WriteLine("Promote logic changed.");
+        }
+
+        //Avoid Protected access modifier unless you have to
+        //private int CalculateRating(bool excludeOrders)
+        //{
+        //    return 0;
+        //}
+    }
+}
