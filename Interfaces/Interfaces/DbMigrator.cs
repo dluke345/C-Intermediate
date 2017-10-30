0699df7cdfc8ee02430ea56f657c5719f7e6e186
David Luke <dluke345@gmail.com>
2017-10-30 14:28:48 -0500

    Add files via upload

---
+++ Interfaces/Interfaces/DbMigrator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Interfaces
+{
+    public class DbMigrator
+    {
+        //Dependcey injection
+        private readonly ILogger _logger;
+
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.LogInfo("Migration started at " + DateTime.Now);
+            //Details of migrating the database
+            _logger.LogError("Migration finished at " + DateTime.Now.AddMinutes(10));
+        }
+    }
+}
